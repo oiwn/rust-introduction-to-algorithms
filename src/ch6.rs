9abeba77903567f9557eab44896c6487750dccbd
oiwn <alex@imscraping.ninja>
2020-09-06 14:38:34 +0000

    heap sort

--- src/ch6.rs
+++ src/ch6.rs
@@ -1,53 +1,62 @@
-fn parent(i: u32) -> u32 {
-    (i / 2) - 1
-}
+// heap sort
 
-fn left(i: u32) -> u32 {
-    (2 * i) - 1
-}
+fn max_heapify(array: &mut [i32], i: usize, heap_size: usize) {
+    let l: usize = (2 * i) + 1;
+    let r: usize = (2 * i) + 2;
+    let mut largest: usize;
 
-fn right(i: u32) -> u32 {
-    2 * i
-}
-
-fn max_heapify(array: &mut [i32], i: u32) {
-    l = left(i);
-    r = right(i);
-    let mut largest: u32 = 0;
-
-    if l < array.len() && array[l] > array[i] {
+    if l < heap_size && array[l] > array[i] {
         largest = l;
     } else {
         largest = i;
     }
 
-    if r <= array.len() && array[r] > array[largest] {
+    if r < heap_size && array[r] > array[largest] {
         largest = r;
     }
 
     if largest != i {
-        array.swap(array[i], array[largest]);
+        array.swap(i, largest);
+        max_heapify(array, largest, heap_size)
     }
-    max_heapify(array, largest)
 }
 
 fn build_max_heap(array: &mut [i32]) {
-    for i in (array.len() / 2)..0 {
-        max_heapify(array, i)
+    let heap_size = array.len();
+    for i in (0..heap_size / 2).rev() {
+        max_heapify(array, i, heap_size);
     }
 }
 
 pub fn heap_sort(array: &mut [i32]) {
+    let mut heap_size: usize = array.len();
     build_max_heap(array);
-    for i in array.len()..1 {
-        array.swap(1, i);
-        max_heapify(array, 1);
+    for i in (1..array.len()).rev() {
+        array.swap(0, i);
+        heap_size -= 1;
+        max_heapify(array, 0, heap_size);
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn max_heapify_test() {
+        let result: [i32; 10] = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1];
+        let mut array: [i32; 10] = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1];
+        max_heapify(&mut array, 1, 10);
+        assert_eq!(array, result);
+    }
+
+    #[test]
+    fn build_max_heap_test() {
+        let result: [i32; 10] = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1];
+        let mut array: [i32; 10] = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7];
+        build_max_heap(&mut array);
+        assert_eq!(array, result);
+    }
 
     #[test]
     fn heap_sort_test() {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod ch1;
+mod ch6;
 
 fn main() {
     // insertion sort
@@ -11,5 +12,11 @@ fn main() {
     let mut array: [i32; 6] = [5, 2, 4, 6, 1, 3];
     println!("Merge sort for array:  {:?}", array);
     ch1::merge_sort(&mut array);
+    println!("Sorted array: {:?}", array);
+
+    // heap sort
+    let mut array: [i32; 6] = [5, 2, 4, 6, 1, 3];
+    println!("Heap sort for array:  {:?}", array);
+    ch6::heap_sort(&mut array);
     println!("Sorted array: {:?}", array);
 }
