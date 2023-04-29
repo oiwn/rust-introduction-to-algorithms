a052ec69e52d7e8d905de68ddecc37352bb68939
oiwn <alex@imscraping.ninja>
2023-04-29 19:00:41 +0700

    docsting for chapter1 sort methods

--- src/ch1.rs
+++ src/ch1.rs
@@ -1,8 +1,8 @@
-/// Two types of sort algorythms from chapter 2
+/// # Two types of sort algorythms from chapter 2
 
 /// Sorts the elements of a mutable slice in ascending order using the insertion sort algorithm.
 ///
-/// # Examples
+/// ## Examples
 ///
 /// ```
 /// let mut array = [5, 2, 4, 6, 1, 3];
@@ -10,11 +10,11 @@
 /// assert_eq!(array, [1, 2, 3, 4, 5, 6]);
 /// ```
 ///
-/// # Parameters
+/// ## Parameters
 ///
 /// * `array` - A mutable slice of elements to be sorted
 ///
-/// # Type Parameters
+/// ## Type Parameters
 ///
 /// * `T` - The type of the elements in the slice. `T` must implement
 /// the `PartialOrd` trait.
@@ -22,7 +22,6 @@ pub fn insertion_sort<T>(array: &mut [T])
 where
     T: PartialOrd,
 {
-    // Insertion sort
     for i in 1..array.len() {
         let mut j = i;
         while j > 0 && array[j - 1] > array[j] {
@@ -32,6 +31,63 @@ where
     }
 }
 
+/// Sorts the elements of a mutable slice in ascending order using the merge sort algorithm.
+///
+/// ## Examples
+///
+/// ```
+/// let mut array = [5, 2, 4, 6, 1, 3];
+/// merge_sort(&mut array);
+/// assert_eq!(array, [1, 2, 3, 4, 5, 6]);
+/// ```
+///
+/// ## Parameters
+///
+/// * `array` - A mutable slice of elements to be sorted
+///
+/// ## Type Parameters
+///
+/// * `T` - The type of the elements in the slice. `T` must implement the `PartialOrd` and `Copy` traits.
+pub fn merge_sort<T>(array: &mut [T])
+where
+    T: PartialOrd + Copy,
+{
+    // merge sort
+    let mid = array.len() / 2;
+    if mid == 0 {
+        return;
+    }
+
+    merge_sort(&mut array[..mid]);
+    merge_sort(&mut array[mid..]);
+
+    let mut ret = array.to_vec();
+    merge(&array[..mid], &array[mid..], &mut ret[..]);
+    array.copy_from_slice(&ret);
+}
+
+/// Merges two slices into a single slice in ascending order.
+///
+/// ## Examples
+///
+/// ```
+/// let arr1 = [1, 3, 5];
+/// let arr2 = [2, 4, 6];
+/// let mut ret = [0; 6];
+///
+/// merge(&arr1, &arr2, &mut ret);
+/// assert_eq!(ret, [1, 2, 3, 4, 5, 6]);
+/// ```
+///
+/// ## Parameters
+///
+/// * `arr1` - A slice of elements to be merged
+/// * `arr2` - A slice of elements to be merged
+/// * `ret` - A mutable slice to store the merged elements
+///
+/// ## Type Parameters
+///
+/// * `T` - The type of the elements in the slice. `T` must implement the `PartialOrd` and `Copy` traits.
 fn merge<T>(arr1: &[T], arr2: &[T], ret: &mut [T])
 where
     T: PartialOrd + Copy,
@@ -55,28 +111,10 @@ where
 
     if left < arr1.len() {
         ret[index..].copy_from_slice(&arr1[left..]);
-    }
+    };
     if right < arr2.len() {
         ret[index..].copy_from_slice(&arr2[right..]);
-    }
-}
-
-pub fn merge_sort<T>(array: &mut [T])
-where
-    T: PartialOrd + Copy,
-{
-    // merge sort
-    let mid = array.len() / 2;
-    if mid == 0 {
-        return;
-    }
-
-    merge_sort(&mut array[..mid]);
-    merge_sort(&mut array[mid..]);
-
-    let mut ret = array.to_vec();
-    merge(&array[..mid], &array[mid..], &mut ret[..]);
-    array.copy_from_slice(&ret);
+    };
 }
 
 #[cfg(test)]
@@ -85,10 +123,17 @@ mod tests {
 
     #[test]
     fn insertion_sort_test() {
-        let result: [i32; 6] = [1, 2, 3, 4, 5, 6];
         let mut array: [i32; 6] = [5, 2, 4, 6, 1, 3];
         insertion_sort(&mut array);
-        assert_eq!(array, result)
+        assert_eq!(array, [1, 2, 3, 4, 5, 6]);
+
+        let mut vec = vec![5.0, 2.3, 4.2, 6.0, 1.4, 3.6];
+        insertion_sort(&mut vec);
+        assert_eq!(vec, [1.4, 2.3, 3.6, 4.2, 5.0, 6.0]);
+
+        let mut vec: Vec<u32> = vec![];
+        insertion_sort(&mut vec);
+        assert_eq!(vec, vec![]);
     }
 
     #[test]
